8ccb9594f2ef5efe3bb023a8619bb5a5d6cbc46c
dENQ <qeqedenq@gmail.com>
2017-08-27 22:36:24 +0200

    setting index

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var express    = require("express"),
+const express    = require("express"),
     app        = express(),
     parser     = require("body-parser");
 
 app.use(express.static('public'))    
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(parser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("index.ejs");
+});
 
 app.listen(3000);
