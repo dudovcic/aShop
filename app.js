14dee5f2b59525c4387452b12885f4a5280de8fb
dENQ <qeqedenq@gmail.com>
2017-08-27 22:45:35 +0200

    switch to node successful

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express    = require("express"),
     app        = express(),
     parser     = require("body-parser");
 
-app.use(express.static('public'))    
+app.use("/static", express.static('public'))    
 app.use(parser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-	res.render("index.ejs");
+	res.render("index");
 });
 
 app.listen(3000);
